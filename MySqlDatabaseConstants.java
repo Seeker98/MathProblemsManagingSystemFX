4c8b04679100277eae6b81cb3d4eb918694239ff
Victor Chen <1157539822@qq.com>
2018-01-31 13:18:54 +0800

    Update MySqlDatabaseConstants.java
    Manually changed the crypted password due to security reasons

--- MySqlDatabaseConstants.java
+++ MySqlDatabaseConstants.java
@@ -8,5 +8,5 @@ public class MySqlDatabaseConstants {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/math_questions";
     static final String USER = "root";
-    static final String PASSWORD_ENCODED="MTIzNDU2dnA=";
+    static final String PASSWORD_ENCODED="MTIzNDU2dof=";
 }
