3d297fcac8aa15f9b4bf28613fe65d6718cddabb
Victor Chen <1157539822@qq.com>
2018-01-31 20:01:47 +0800

    Update MySqlDatabaseConstants.java

--- MySqlDatabaseConstants.java
+++ MySqlDatabaseConstants.java
@@ -8,5 +8,5 @@ public class MySqlDatabaseConstants {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/math_questions";
     static final String USER = "root";
-    static final String PASSWORD_ENCODED="MTIzNDU2dof=";
+    static final String PASSWORD_ENCODED="OTIzNEU2dof=";
 }
